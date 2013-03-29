e54a344bb7fd74d2186b525256f0a62eb4286eac
Samir Remtulla <samir.remtulla@gmail.com>
2013-03-29 12:28:14 -0400

    Initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamirTable::Application.config.secret_token = 'a47e50e7d64c1454d5b7d91e41702fd264823401f925d59ba0d571d43912a37940bdcaa74ee1f86f8088bd43c413bbaba20cae60c6c6a8e70d573cfbebda7015'
